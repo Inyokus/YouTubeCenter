a1abedb29c93097d4e802717993bf69943ca74f7
Inyokus <Inyokus@users.noreply.github.com>
2017-03-04 17:38:47 +0100

    improved build file, added unfinished build target for Firefox

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,132 @@
 module.exports = function(grunt) {
-    var properties = grunt.file.readJSON('properties.json');
-    properties["ant-database-language"] = grunt.file.read("language.json");
 
+    // Initialize properties (app name, translations, ...)
+    var properties = grunt.file.readJSON("properties.json");
+    properties['ant-database-language'] = grunt.file.read("language.json");
 
+    // Constants
+    var IDENTIFIERS = {
+        userscript: 0,
+        chrome: 1,
+        maxthon: 2,
+        safari: 4,
+        opera: 5,
+        firefox: 6,
+        scriptish: 7,
+        chrome_webstore: 8
+    };
 
+    var PATHS = {
+        src: "src/",
+        build: "grunt/",
+        dist: "grunt/"
+    };
+    PATHS.styles = {
+        srcDir: PATHS.src + ".styles/",
+        buildDir: PATHS.build + "styles/"
+    };
+    PATHS.common = {
+        src: PATHS.src + "YouTubeCenter.user.js",
+        build: PATHS.build + "YouTubeCenter.user.js"
+    };
+    PATHS.userscript = {
+        src: PATHS.src + "YouTubeCenter.user.js",
+        build: PATHS.build + "YouTubeCenter.user.js",
+        dist: PATHS.dist + "YouTubeCenter.user.js"
+    };
+    PATHS.userscript_meta = {
+        src: PATHS.src + "user_script_header.js",
+        dist: PATHS.dist + "YouTubeCenter.meta.js"
+    };
+    PATHS.firefox = {
+        srcDir: PATHS.src + ".firefox/",
+        dist: PATHS.dist + "YouTubeCenter.xpi"
+    };
+
+    // Task configuration
     grunt.initConfig({
+        set_identifier: IDENTIFIERS,
         preprocess: {
-            dist: {
-                src: "src/YouTubeCenter.user.js",
-                dest: "grunt/YouTubeCenter.user.js"
+            build: {
+                src: PATHS.common.src,
+                dest: PATHS.common.build
             }
         },
         replace: {
-            dist: {
-                src: "grunt/YouTubeCenter.user.js",
-                overwrite: true,
-                replacements: [{
-                    from: /\@([a-zA-Z0-9\-_]*?)\@/g,
-                    to: function (matchedWord, index, fullText, regexMatches) {
-                        if (properties[regexMatches[0]] !== undefined) {
-                            return properties[regexMatches[0]]
+            options: {
+                patterns: [{
+                    match: /@([a-zA-Z0-9\-_]*?)@/g,
+                    replacement: function (fullMatch, key) {
+                        if (properties[key] !== undefined) {
+                            return properties[key]
                         } else {
-                            grunt.log.writeln("Key not found: " + regexMatches[0]);
-                            return matchedWord;
+                            grunt.log.writeln("Key not found: " + key);
+                            return fullMatch;
                         }
                     }
                 }]
+            },
+            dist: {
+                src: PATHS.userscript.build,
+                dest: PATHS.userscript.dist
+            },
+            meta: {
+                src: PATHS.userscript_meta.src,
+                dest: PATHS.userscript_meta.dist
+            },
+            build: {
+                src: PATHS.common.build,
+                dest: PATHS.common.build
             }
         },
         cssmin: {
             dist: {
                 expand: true,
-                cwd: "src/.styles/",
+                cwd: PATHS.styles.srcDir,
                 src: "*.css",
-                dest: "grunt/styles/"
+                dest: PATHS.styles.buildDir
+            }
+        },
+        compress: {
+            firefox: {
+                options: {
+                    mode: 'zip',
+                    archive: PATHS.firefox.dist
+                },
+                files: {
+                    src: [PATHS.common.build, PATHS.firefox.srcDir + "*"]
+                }
             }
         }
     });
 
+    //Load/Set up tasks that have a configuration
     grunt.loadNpmTasks('grunt-preprocess');
-    grunt.loadNpmTasks('grunt-text-replace');
-    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.registerMultiTask('set_identifier', function () {
+        properties.identifier = this.data;
+    });
 
-    grunt.registerTask("load-css", function () {
-        grunt.file.expandMapping("grunt/styles/*.css", "", {ext: "", flatten: true}).forEach(function (file) {
-            properties["styles-" + file.dest] = grunt.file.read(file.src);
+
+    // Register function only tasks
+    grunt.registerTask('load-css', function () {
+        grunt.file.expandMapping(PATHS.styles.buildDir + "*.css", "", {ext: "", flatten: true}).forEach(function (file) {
+            properties['styles-' + file.dest] = grunt.file.read(file.src);
         });
     });
 
-    grunt.registerTask('userscript' , ["cssmin", "load-css", "preprocess", "replace"])
+
+    // Common tasks
+    grunt.registerTask('for_all', ['preprocess', 'cssmin', 'load-css']);
+    grunt.registerTask('common', ['for_all', 'replace:build']);
+
+    // Userscript tasks
+    grunt.registerTask('userscript_bare', ['set_identifier:userscript', 'for_all', 'replace:dist']);
+    grunt.registerTask('userscript_meta', ['replace:meta']);
+
+    // Public tasks
+    grunt.registerTask('firefox', ['set_identifier:firefox', 'common', 'compress:firefox']);
+    grunt.registerTask('userscript', ['userscript_bare', 'userscript_meta']);
 };
